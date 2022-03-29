afee3598bccb8101a75b2c0d9b7b4947018329b8
BXV2000 <82429306+BXV2000@users.noreply.github.com>
2022-03-29 19:58:07 +0700

    adding more pages

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div className="About">
+        <div className="content">This is about</div>
+    </div>
+  )
+}
+
+export default About
