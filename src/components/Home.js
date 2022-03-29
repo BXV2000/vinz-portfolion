97f482b6be4e2aa67b87ccf337a3f54df6a52bce
BXV2000 <82429306+BXV2000@users.noreply.github.com>
2022-03-29 19:13:52 +0700

    Change directory name

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import '../css/Home.css';
+import Navbar from './Navbar';
+
+function Home() {
+  return (
+    <div className="Home">
+      <Navbar />
+      <div className="content">
+        <h1>Testing Component</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
